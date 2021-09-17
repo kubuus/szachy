9915c310756609f409783b5a2a1ffadf0521c515
kubuus <kubaj66@gmail.com>
2021-09-17 23:50:04 +0200

    Changed arguments to Move class

--- move.cpp
+++ move.cpp
@@ -3,34 +3,35 @@
 // TODO: Create a method for checking if a square is attacked, then implement it
 //       for king moves. Implement moves of the rest of pieces.
 
-bool Position::IsLegal(eSquares StartingSq, eSquares TargetSq, ePieceType PieceType)
+bool Position::IsLegal(Move Move)
 {
     Bitboards BB;
     BB.Init();
     int Col = Rest & U64(1);
 
-    if(!(StartingSq & Colour_BB[Col]) || !(StartingSq & Piece_BB[PieceType]))
+    if(!(Move.MoveFrom & Colour_BB[Col]) || !(Move.MoveFrom & Piece_BB[Move.PieceType]))
         return false;
     
-    if(TargetSq & Colour_BB[Col])
+    if(Move.MoveTo & Colour_BB[Col])
         return false;
 
-    eMoveType movetype;
-
-    if (PieceType == K)
+    if (Move.PieceType == K)
     {
-        if(!(BB.GetKingAttacks(StartingSq) & TargetSq))
+        if(!(BB.GetKingAttacks(Move.MoveFrom) & Move.MoveTo))
         {
+            
+            if(Move.MoveType > QUIET)
+            {
             int CR = CastlingRights(Col);
             if(CR == 0)
                 return false;
             
-            if((TargetSq == g8 - (!Col * a8)) && (CR & (1)) && ((GetPos(NC, NPT) & (K_CASTLE_MASK << North * 7 * !Col)) == 0))
+            if((Move.MoveTo == g8 - (!Col * a8)) && (CR & (1)) && ((GetPos(NC, NPT) & (K_CASTLE_MASK << North * 7 * !Col)) == 0))
                 return true;
             
-            if((TargetSq == c8 - (!Col * a8)) && (CR & (2)) && ((GetPos(NC, NPT) & (Q_CASTLE_MASK << North * 7 * !Col)) == 0))
+            if((Move.MoveTo == c8 - (!Col * a8)) && (CR & (2)) && ((GetPos(NC, NPT) & (Q_CASTLE_MASK << North * 7 * !Col)) == 0))
                 return true;
-            
+            }
             return false;
         }
         
@@ -39,10 +40,10 @@ bool Position::IsLegal(eSquares StartingSq, eSquares TargetSq, ePieceType PieceT
     return true;
 }
 
-void Position::Move(eSquares StartingSq, eSquares TargetSq, ePieceType PieceType)
+void Position::MakeMove(Move Move)
 {
-    U64 bb = sq((U64)(StartingSq)) | sq((U64)(TargetSq));
+    U64 bb = sq((U64)(Move.MoveFrom)) | sq((U64)(Move.MoveTo));
     Colour_BB[Rest & U64(1)] ^= bb;
-    Piece_BB[PieceType] ^= bb;
+    Piece_BB[Move.PieceType] ^= bb;
     Rest ^= U64(1);
 }
