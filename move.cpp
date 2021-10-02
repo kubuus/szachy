c4e1f0bb4d93553174212377f108ae71585c684f
kubuus <kubaj66@gmail.com>
2021-10-03 00:39:10 +0200

    Finished MakeMove method

--- move.cpp
+++ move.cpp
@@ -1,25 +1,47 @@
 #include "types.h"
 
-// TODO: Implement moves of the rest of pieces.
+// TODO: Implement undo move method.
 
 
 // Method only changes bitboards getting the Move class 
 // This method DOES NOT check whether the move is legal
 void Position::MakeMove(Move MoveDo, Move *Undo)
 {
-    // Saving undo data to only if it's at NULL value
+    // Saving undo data only if it's at NULL value
     if(!Undo)
-        Undo->Init(MoveDo.MoveTo, MoveDo.MoveFrom, QUIET, MoveDo.PieceType, n_sq);
+        Undo->Init(MoveDo.MoveTo, MoveDo.MoveFrom, QUIET, MoveDo.PieceType, EPsq);
 
-    U64 bb = sq((U64)(MoveDo.MoveFrom)) | (sq((U64)(MoveDo.MoveTo)));
+    U64 bb = sq(MoveDo.MoveFrom) | (sq(MoveDo.MoveTo));
     Colour_BB[Turn] ^= bb;
-    if(Colour_BB[~Turn] & sq((U64)(MoveDo.MoveTo))){
-        
-        Colour_BB[~Turn] ^ sq((U64)(MoveDo.MoveTo));
-        }
     Piece_BB[MoveDo.PieceType] ^= bb;
+
+    if(Colour_BB[~Turn] & sq(MoveDo.MoveTo))
+    {
+        Piece_BB[PieceList[MoveDo.MoveTo] - 6] ^= sq(MoveDo.MoveTo);
+        Colour_BB[~Turn] ^= sq(MoveDo.MoveTo);
+    }
+    if(CastlingRights(Turn) != 0)
+    {
+        if(MoveDo.PieceType == K)
+            CastRights ^= 3 << (2 * Turn);
+
+        if(MoveDo.PieceType == R)
+        {
+            if((CastlingRights(Turn) & 1) && (MoveDo.MoveFrom == (a1 << (56 * Turn))))
+                CastRights ^= 1 << (2 * Turn);
+            
+            if((CastlingRights(Turn) & 2) && (MoveDo.MoveFrom == (h1 << (56 * Turn))))
+                CastRights ^= 2 << (2 * Turn);
+        }
+    }
+
+    if((MoveDo.MoveType != CAPTURE) && (MoveDo.PieceType != P))
+        ply++; else ply = 0;
+    
     Turn = ~Turn;
+    if(Turn == White) move_no++;
     EPsq = n_sq;
+
     if(MoveDo.MoveType == DOUBLE_PAWN_PUSH)
         EPsq = eSquares(MoveDo.MoveTo - a2);
 }
@@ -32,16 +54,20 @@ bool Position::IsLegal(Move MoveDo)
     if((MoveDo.MoveTo & Colour_BB[Turn]) || !(MoveDo.MoveTo & BB_Misc.GetAttacks(MoveDo.PieceType, MoveDo.MoveFrom, Colour_BB[NC], Turn)))
         return false;
     
-    Move *Undo = NULL;
-    MakeMove(MoveDo, Undo);
+    if(MoveDo.PieceType == K && AttackedSquare(MoveDo.MoveTo, ~Turn))
+        return false;
+
+    else {
+    Colour_BB[Turn] ^= sq(MoveDo.MoveFrom);
 
     if(AttackedSquare(eSquares(TrailingZeros(GetPos(Turn, K))), ~Turn))
     {
-        MakeMove(*Undo, &MoveDo);
+        Colour_BB[Turn] ^= sq(MoveDo.MoveFrom);
         return false;
     }
     
-    MakeMove(*Undo, &MoveDo);
+    Colour_BB[Turn] ^= sq(MoveDo.MoveFrom);
+    }
 
     switch(MoveDo.MoveType)
     {
