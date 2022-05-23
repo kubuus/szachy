f7892fb9f62ecbd2a96aab8b9059488fb44189b7
kubuus <kubaj66@gmail.com>
2022-05-23 16:18:21 +0200

    Typo

--- eval.cpp
+++ eval.cpp
@@ -3,6 +3,6 @@
 void Vertex::Evaluate()
 {
 	float PieceValues[6] = { 1, 3, 3, 5, 8, 200 };
-	for (ePieceType i = P, i < NPT, i++)
+	for (ePieceType i = P; i < NPT; i++)
 		Score += PopCount(Pos.GetPos(Pos.Turn, i)) * PieceValues[i];
 }
